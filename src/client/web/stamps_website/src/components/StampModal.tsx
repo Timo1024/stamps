99afbee478cc898dd01984404241b3e3f6790516
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2024-12-16 23:42:56 +0100

    made stamp info in modal editable

--- src/client/web/stamps_website/src/components/StampModal.tsx
+++ src/client/web/stamps_website/src/components/StampModal.tsx
@@ -13,6 +13,7 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
     const [image, setImage] = useState<File | null>(null);
     const [isVisible, setIsVisible] = useState(false);
     const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const modalRef = useRef<HTMLDivElement>(null);
     const [stampDetails, setStampDetails] = useState<{ [key: string]: string }>({});
     const [stampValuesAndOwnership, setStampValuesAndOwnership] = useState<{ [key: string]: [string | null, number | null, number | null] }>({});
@@ -73,7 +74,7 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
             "Year": stamp.year ? stamp.year : "-",
             "Denomination": stamp.denomination ? stamp.denomination : "-",
             "Themes": themes,
-            "Last added": lastAdded,
+            // "Last added": lastAdded,
             "Category": stamp.category ? stamp.category : "-",
             "Color": stamp.color ? stamp.color : "-",
             "Date of Issue": dateOfIssue,
@@ -127,6 +128,23 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
         }, 0);
     };
 
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleSaveClick = () => {
+        setIsEditing(false);
+        // Save the updated stamp details
+        onSave({ ...stamp, ...stampDetails });
+    };
+
+    const handleInputChange = (key: string, value: string) => {
+        setStampDetails((prevDetails) => ({
+            ...prevDetails,
+            [key]: value,
+        }));
+    };
+
     return (
         <div className={`stamp-modal-overlay ${isVisible ? 'visible' : ''}`}>
             <div className="stamp-modal" ref={modalRef}>
@@ -135,6 +153,16 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
                         <path d="M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L4 3.29289L6.64645 0.646447C6.84171 0.451184 7.15829 0.451184 7.35355 0.646447C7.54882 0.841709 7.54882 1.15829 7.35355 1.35355L4.70711 4L7.35355 6.64645C7.54882 6.84171 7.54882 7.15829 7.35355 7.35355C7.15829 7.54882 6.84171 7.54882 6.64645 7.35355L4 4.70711L1.35355 7.35355C1.15829 7.54882 0.841709 7.54882 0.646447 7.35355C0.451184 7.15829 0.451184 6.84171 0.646447 6.64645L3.29289 4L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z" fill="#fff" />
                     </svg>
                 </button>
+                <button className="edit-button" onClick={handleEditClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                        <path d="M12.1465 0.146447C12.3417 -0.0488155 12.6583 -0.0488155 12.8536 0.146447L15.8536 3.14645C16.0488 3.34171 16.0488 3.65829 15.8536 3.85355L5.85357 13.8536C5.80569 13.9014 5.74858 13.9391 5.68571 13.9642L0.68571 15.9642C0.500001 16.0385 0.287892 15.995 0.146461 15.8536C0.00502989 15.7121 -0.0385071 15.5 0.0357762 15.3143L2.03578 10.3143C2.06092 10.2514 2.09858 10.1943 2.14646 10.1464L12.1465 0.146447ZM11.2071 2.5L13.5 4.79289L14.7929 3.5L12.5 1.20711L11.2071 2.5ZM12.7929 5.5L10.5 3.20711L4.00001 9.70711V10H4.50001C4.77616 10 5.00001 10.2239 5.00001 10.5V11H5.50001C5.77616 11 6.00001 11.2239 6.00001 11.5V12H6.29291L12.7929 5.5ZM3.03167 10.6755L2.92614 10.781L1.39754 14.6025L5.21903 13.0739L5.32456 12.9683C5.13496 12.8973 5.00001 12.7144 5.00001 12.5V12H4.50001C4.22387 12 4.00001 11.7761 4.00001 11.5V11H3.50001C3.28561 11 3.10272 10.865 3.03167 10.6755Z" fill="black"/>
+                    </svg>
+                </button>
+                {isEditing && (
+                    <button className="save-button" onClick={handleSaveClick}>
+                        Save
+                    </button>
+                )}
                 <div className="stamp-modal-content">
                     <h2>{stamp.set_name}</h2>
                     <div className="stamp-modal-image-container">
@@ -145,14 +173,26 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
                             onLoad={handleImageLoad}
                         />
                     </div>
+                    {/* make horizontal line */}
+                    <hr className="modal-line" />
                     <div className="stamp-details">
                         {Object.keys(stampDetails).map((key) => (
                             <div key={key} className="stamp-detail">
                                 <div className="stamp-detail-label">{key}</div>
-                                <div className="stamp-detail-value">{stampDetails[key]}</div>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={stampDetails[key]}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        className="stamp-detail-input"
+                                    />
+                                ) : (
+                                    <div className="stamp-detail-value">{stampDetails[key]}</div>
+                                )}
                             </div>
                         ))}
                     </div>
+                    <hr className="modal-line" />
                     <div className="stamp-values-table">
                         <table>
                             <thead>
