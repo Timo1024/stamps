a2146765bc77eabd924a0735034891d4a214aa7f
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2024-12-17 00:31:37 +0100

    changed edit toggle style

--- src/client/web/stamps_website/src/components/StampModal.tsx
+++ src/client/web/stamps_website/src/components/StampModal.tsx
@@ -153,16 +153,6 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
                         <path d="M0.646447 0.646447C0.841709 0.451184 1.15829 0.451184 1.35355 0.646447L4 3.29289L6.64645 0.646447C6.84171 0.451184 7.15829 0.451184 7.35355 0.646447C7.54882 0.841709 7.54882 1.15829 7.35355 1.35355L4.70711 4L7.35355 6.64645C7.54882 6.84171 7.54882 7.15829 7.35355 7.35355C7.15829 7.54882 6.84171 7.54882 6.64645 7.35355L4 4.70711L1.35355 7.35355C1.15829 7.54882 0.841709 7.54882 0.646447 7.35355C0.451184 7.15829 0.451184 6.84171 0.646447 6.64645L3.29289 4L0.646447 1.35355C0.451184 1.15829 0.451184 0.841709 0.646447 0.646447Z" fill="#fff" />
                     </svg>
                 </button>
-                <button className="edit-button" onClick={handleEditClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                        <path d="M12.1465 0.146447C12.3417 -0.0488155 12.6583 -0.0488155 12.8536 0.146447L15.8536 3.14645C16.0488 3.34171 16.0488 3.65829 15.8536 3.85355L5.85357 13.8536C5.80569 13.9014 5.74858 13.9391 5.68571 13.9642L0.68571 15.9642C0.500001 16.0385 0.287892 15.995 0.146461 15.8536C0.00502989 15.7121 -0.0385071 15.5 0.0357762 15.3143L2.03578 10.3143C2.06092 10.2514 2.09858 10.1943 2.14646 10.1464L12.1465 0.146447ZM11.2071 2.5L13.5 4.79289L14.7929 3.5L12.5 1.20711L11.2071 2.5ZM12.7929 5.5L10.5 3.20711L4.00001 9.70711V10H4.50001C4.77616 10 5.00001 10.2239 5.00001 10.5V11H5.50001C5.77616 11 6.00001 11.2239 6.00001 11.5V12H6.29291L12.7929 5.5ZM3.03167 10.6755L2.92614 10.781L1.39754 14.6025L5.21903 13.0739L5.32456 12.9683C5.13496 12.8973 5.00001 12.7144 5.00001 12.5V12H4.50001C4.22387 12 4.00001 11.7761 4.00001 11.5V11H3.50001C3.28561 11 3.10272 10.865 3.03167 10.6755Z" fill="black"/>
-                    </svg>
-                </button>
-                {isEditing && (
-                    <button className="save-button" onClick={handleSaveClick}>
-                        Save
-                    </button>
-                )}
                 <div className="stamp-modal-content">
                     <h2>{stamp.set_name}</h2>
                     <div className="stamp-modal-image-container">
@@ -175,22 +165,38 @@ const StampModal: React.FC<StampModalProps> = ({ stamp, onClose, onSave }) => {
                     </div>
                     {/* make horizontal line */}
                     <hr className="modal-line" />
-                    <div className="stamp-details">
-                        {Object.keys(stampDetails).map((key) => (
-                            <div key={key} className="stamp-detail">
-                                <div className="stamp-detail-label">{key}</div>
-                                {isEditing ? (
-                                    <input
-                                        type="text"
-                                        value={stampDetails[key]}
-                                        onChange={(e) => handleInputChange(key, e.target.value)}
-                                        className="stamp-detail-input"
-                                    />
-                                ) : (
-                                    <div className="stamp-detail-value">{stampDetails[key]}</div>
-                                )}
-                            </div>
-                        ))}
+                    <div className="stamp-details-wrapper">
+                        <div className="stamp-details">
+                            {Object.keys(stampDetails).map((key) => (
+                                <div key={key} className="stamp-detail">
+                                    <div className="stamp-detail-label">{key}</div>
+                                    {isEditing ? (
+                                        <input
+                                            type="text"
+                                            value={stampDetails[key]}
+                                            onChange={(e) => handleInputChange(key, e.target.value)}
+                                            className="stamp-detail-input"
+                                        />
+                                    ) : (
+                                        <div className="stamp-detail-value">{stampDetails[key]}</div>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                        {!isEditing && (
+                            <button className="edit-button" onClick={handleEditClick}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                    <path d="M12.1465 0.146447C12.3417 -0.0488155 12.6583 -0.0488155 12.8536 0.146447L15.8536 3.14645C16.0488 3.34171 16.0488 3.65829 15.8536 3.85355L5.85357 13.8536C5.80569 13.9014 5.74858 13.9391 5.68571 13.9642L0.68571 15.9642C0.500001 16.0385 0.287892 15.995 0.146461 15.8536C0.00502989 15.7121 -0.0385071 15.5 0.0357762 15.3143L2.03578 10.3143C2.06092 10.2514 2.09858 10.1943 2.14646 10.1464L12.1465 0.146447ZM11.2071 2.5L13.5 4.79289L14.7929 3.5L12.5 1.20711L11.2071 2.5ZM12.7929 5.5L10.5 3.20711L4.00001 9.70711V10H4.50001C4.77616 10 5.00001 10.2239 5.00001 10.5V11H5.50001C5.77616 11 6.00001 11.2239 6.00001 11.5V12H6.29291L12.7929 5.5ZM3.03167 10.6755L2.92614 10.781L1.39754 14.6025L5.21903 13.0739L5.32456 12.9683C5.13496 12.8973 5.00001 12.7144 5.00001 12.5V12H4.50001C4.22387 12 4.00001 11.7761 4.00001 11.5V11H3.50001C3.28561 11 3.10272 10.865 3.03167 10.6755Z" fill="#fff" />
+                                </svg>
+                            </button>
+                        )}
+                        {isEditing && (
+                            <button className="save-button" onClick={handleSaveClick}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="10" viewBox="0 0 11 10" fill="none">
+                                    <path d="M9.73649 0.96967C10.0255 0.676777 10.4942 0.676777 10.7832 0.96967C11.0687 1.25897 11.0722 1.72582 10.7937 2.01947L4.88025 9.00973C4.87456 9.01693 4.86848 9.02381 4.86205 9.03033C4.573 9.32322 4.10437 9.32322 3.81532 9.03033L0.216784 5.38388C-0.0722613 5.09099 -0.0722613 4.61612 0.216784 4.32322C0.505829 4.03033 0.974464 4.03033 1.26351 4.32322L4.31638 7.41674L9.71686 0.992105C9.72295 0.984235 9.72951 0.976743 9.73649 0.96967Z" fill="#fff"/>
+                                </svg>
+                            </button>
+                        )}
                     </div>
                     <hr className="modal-line" />
                     <div className="stamp-values-table">
